b966f4c42a6b6589e56f5aac7b9b3657508cbc43
Gregory Wilson <gjwilson7390@gmail.com>
2020-09-10 17:45:08 -0500

    At mvp and have 3 of the endpoints working (stretch goals). Just have last endpoint to write.

--- recipes/recipe-model.js
+++ recipes/recipe-model.js
@@ -8,15 +8,27 @@ module.exports = {
 
 
 function getRecipes() {
-
+    return db('recipes')
 }
 
 
 function getShoppingList(recipe_id) {
 
+    return db('recipe_ingredients')
+        .join('recipes', 'recipe_ingredients.recipe_id', '=', 'recipes.id')
+        .select('recipes.recipe_name')
+        .join('ingredients', 'recipe_ingredients.ingredient_id', '=', 'ingredients.id')
+        .select('ingredients.ingredient_name', 'recipe_ingredients.ingredient_quantity')
+        
+        .where({ 'recipe_ingredients.recipe_id': recipe_id })
 }
 
 
 function getInstructions(recipe_id) {
+
+    return db('instructions')
+        .join('recipes', 'instructions.recipe_id', '=', 'recipes.id')
+        .select('recipes.recipe_name', 'instructions.instruction')
+        .where({ 'instructions.recipe_id': recipe_id})
 
 }
--- recipes/recipe-router.js
+++ recipes/recipe-router.js
@@ -4,127 +4,54 @@ const Recipes = require('./recipe-model.js');
 
 const router = express.Router();
 
-// router.get('/', (req, res) => {
-//   Recipes.find()
-//   .then(schemes => {
-//     res.json(schemes);
-//   })
-//   .catch(err => {
-//     res.status(500).json({ message: 'Failed to get schemes' });
-//   });
-// });
+
+// Get recipes
+router.get('/', (req, res) => {
+  Recipes.getRecipes()
+  .then(recipes => {
+    res.json(recipes);
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get recipes' });
+  });
+});
+
+
+// Get recipe name, and the corresponding ingredients and quantity of ingredients
+router.get('/:id/shoppingList', (req, res) => {
+  const { id } = req.params;
+
+  Recipes.getShoppingList(id)
+  .then(ingredient => {
+    if (ingredient) {
+      res.json(ingredient);
+    } else {
+      res.status(404).json({ message: 'Could not find recipe ingredients.' })
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get recipe ingredients.' });
+  });
+});
 
 
 
-// router.get('/:id', (req, res) => {
-//   const { id } = req.params;
+router.get('/:id/instructions', (req, res) => {
+    const {id} = req.params
 
-//   Recipes.findById(id)
-//   .then(scheme => {
-//     if (scheme) {
-//       res.json(scheme);
-//     } else {
-//       res.status(404).json({ message: 'Could not find scheme with given id.' })
-//     }
-//   })
-//   .catch(err => {
-//     res.status(500).json({ message: 'Failed to get schemes' });
-//   });
-// });
+    Recipes.getInstructions(id)
+        .then(instruction => {
+            if (instruction) {
+                res.json(instruction)
+            } else {
+                res.status(404).json({ message: 'Could not find recipe instructions.' })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: err.message });
+          });
+})
 
-
-
-
-// router.get('/:id/steps', (req, res) => {
-//   const { id } = req.params;
-
-//   Recipes.findSteps(id)
-//   .then(steps => {
-//     if (steps.length) {
-//       res.json(steps);
-//     } else {
-//       res.status(404).json({ message: 'Could not find steps for given scheme' })
-//     }
-//   })
-//   .catch(err => {
-//     res.status(500).json({ message: 'Failed to get steps' });
-//   });
-// });
-
-
-
-// router.post('/', (req, res) => {
-//   const recipeData = req.body;
-
-//   Recipes.add(recipeData)
-//   .then(scheme => {
-//     res.status(201).json(scheme);
-//   })
-//   .catch (err => {
-//     res.status(500).json({ message: 'Failed to create new scheme' });
-//   });
-// });
-
-
-
-
-// router.post('/:id/steps', (req, res) => {
-//   const stepData = req.body;
-//   const { id } = req.params; 
-
-//   Recipes.findById(id)
-//   .then(scheme => {
-//     if (scheme) {
-//       Schemes.addStep(stepData, id)
-//       .then(step => {
-//         res.status(201).json(step);
-//       })
-//     } else {
-//       res.status(404).json({ message: 'Could not find scheme with given id.' })
-//     }
-//   })
-//   .catch (err => {
-//     res.status(500).json({ message: 'Failed to create new step' });
-//   });
-// });
-
-
-
-// router.put('/:id', (req, res) => {
-//   const { id } = req.params;
-//   const changes = req.body;
-
-//   Recipes.findById(id)
-//   .then(scheme => {
-//     if (scheme) {
-//       Schemes.update(changes, id)
-//       .then(updatedScheme => {
-//         res.json(updatedScheme);
-//       });
-//     } else {
-//       res.status(404).json({ message: 'Could not find scheme with given id' });
-//     }
-//   })
-//   .catch (err => {
-//     res.status(500).json({ message: 'Failed to update scheme' });
-//   });
-// });
-
-// router.delete('/:id', (req, res) => {
-//   const { id } = req.params;
-
-//   Recipes.remove(id)
-//   .then(deleted => {
-//     if (deleted) {
-//       res.json({ removed: deleted });
-//     } else {
-//       res.status(404).json({ message: 'Could not find scheme with given id' });
-//     }
-//   })
-//   .catch(err => {
-//     res.status(500).json({ message: 'Failed to delete scheme' });
-//   });
-// });
 
 
 module.exports = router;
